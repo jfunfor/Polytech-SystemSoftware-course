b72918506af404b9cc59e38428c387d8ec6754c2
unknown <ddkudrin@mail.ru>
2025-09-18 16:18:44 +0300

    Изменил подписку на события в редисе

--- backend/vm-manager/src/vm_manager/services/vm_timer.py
+++ backend/vm-manager/src/vm_manager/services/vm_timer.py
@@ -41,7 +41,7 @@ async def vm_cleanup_worker(tf_manager):
     print("Воркер очистки VM (pub/sub) запущен", flush=True)
     
     pubsub = async_redis_client.pubsub()
-    await pubsub.psubscribe('__keyevent@0__:expired')
+    await pubsub.psubscribe('__keyevent@1__:expired')
     
     try:
         async for message in pubsub.listen():
